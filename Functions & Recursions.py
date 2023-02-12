b9c13d51e68188b25a1f5e7f4efe2947a4c1ae53
Marij Murtaza <112205127+marijmurtaza@users.noreply.github.com>
2023-02-12 12:15:13 +0500

    I have made some changes and added recursion

--- Functions & Recursions.py
+++ Functions & Recursions.py
@@ -9,3 +9,33 @@ def greet_user():
     print("Good day, " + name)
 
 greet_user()
+# There are two types of Functions in Python 1.Built in functions 2.User defined functions 
+
+#Function with arguments
+def add(a, b):
+    return a + b
+
+result = add(10, 20)
+print(result)  
+
+'''A default parameter value in a function in Python is a value that is assigned to a parameter when no value is provided for that parameter in a function call. 
+If a value is provided, the default value is overridden.
+'''
+def rectangle_area(length, width=10):
+    return length * width
+
+area = rectangle_area(20)
+print(area)  
+
+area = rectangle_area(20, 30)
+print(area) 
+
+#Recursion 
+#Recursion is a technique in computer programming where a function calls itself as a subroutine
+
+def factorial(n):
+  if n == 0:
+    return 1
+  else:
+    return n * factorial(n-1)
+
